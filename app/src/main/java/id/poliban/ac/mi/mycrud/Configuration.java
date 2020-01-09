5239586802288ca6753a3409db4b10cc7c976f04
Ryanda-Utama <thanatosku@gmail.com>
2020-01-09 14:26:39 +0800

    Initial commit

---
+++ app/src/main/java/id/poliban/ac/mi/mycrud/Configuration.java
@@ -0,0 +1,24 @@
+package id.poliban.ac.mi.mycrud;
+
+public class Configuration {
+
+    public static final String URL_ADD = "http://192.168.43.57/employee/InsertPegawai.php";
+    public static final String URL_GET_ALL = "http://192.168.43.57/employee/GetAllPegawa.php";
+    public static final String URL_GET_EMP = "http://192.168.43.57/employee/GetPegawai.php?id=";
+    public static final String URL_UPDATE_EMP = "http://192.168.43.57/employee/UpdatePegawai.php";
+    public static final String URL_DELETE_EMP = "http://192.168.43.57/employee/DeletePegawai.php?id=";
+
+    public static final String KEY_EMP_ID = "id";
+    public static final String KEY_EMP_NAMA = "nama";
+    public static final String KEY_EMP_POSISI = "posisi";
+    public static final String KEY_EMP_GAJI = "gaji";
+
+    public static final String TAG_JSON_ARRAY= "data";
+    public static final String TAG_ID= "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_POSISI= "posisi";
+    public static final String TAG_GAJI = "gaji";
+
+    public static final String EMP_ID = "emp_id";
+
+}
